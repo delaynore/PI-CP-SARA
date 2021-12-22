c78045f9444594cac7c7b366835e9da552b3eaeb
delaynore <delaynore@gmail.com>
2021-12-22 11:03:02 +0400

    created, without code, MainForm

---
+++ MainForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace MyApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.CreateAdvertButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.InfoButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.OpenButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.OpenButton.Location = new System.Drawing.Point(269, 12);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(117, 38);
+            this.OpenButton.TabIndex = 0;
+            this.OpenButton.Text = "Открыть";
+            this.OpenButton.UseVisualStyleBackColor = false;
+            // 
+            // CreateAdvertButton
+            // 
+            this.CreateAdvertButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.CreateAdvertButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.CreateAdvertButton.Location = new System.Drawing.Point(269, 56);
+            this.CreateAdvertButton.Name = "CreateAdvertButton";
+            this.CreateAdvertButton.Size = new System.Drawing.Size(117, 41);
+            this.CreateAdvertButton.TabIndex = 1;
+            this.CreateAdvertButton.Text = "Создать обьявление";
+            this.CreateAdvertButton.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MyApp.Resource1.mainIcon;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(216, 178);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseMove);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ExitButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ExitButton.Location = new System.Drawing.Point(269, 156);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(117, 34);
+            this.ExitButton.TabIndex = 3;
+            this.ExitButton.Text = "Выход";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            // 
+            // InfoButton
+            // 
+            this.InfoButton.BackColor = System.Drawing.SystemColors.Info;
+            this.InfoButton.Location = new System.Drawing.Point(269, 103);
+            this.InfoButton.Name = "InfoButton";
+            this.InfoButton.Size = new System.Drawing.Size(117, 24);
+            this.InfoButton.TabIndex = 4;
+            this.InfoButton.Text = "О приложении";
+            this.InfoButton.UseVisualStyleBackColor = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 202);
+            this.ControlBox = false;
+            this.Controls.Add(this.InfoButton);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.CreateAdvertButton);
+            this.Controls.Add(this.OpenButton);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button OpenButton;
+        private Button CreateAdvertButton;
+        private PictureBox pictureBox1;
+        private Button ExitButton;
+        private Button InfoButton;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private Point MouseHook;
+        private void MainForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            Cursor = Cursors.Default;
+            if (e.Button != MouseButtons.Left) MouseHook = e.Location;
+            else
+            {
+                Location = new Point((Size)Location - (Size)MouseHook + (Size)e.Location);
+                Cursor = Cursors.Hand;
+            }
+        }
+    }
+}
