12fd8755285fdc86c4ce176dd6513fb19680fd50
delaynore <delaynore@gmail.com>
2021-12-22 16:31:39 +0400

    created class advertisiment

---
+++ Advertisiment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+    class Advertisiment
+    {
+        public Advertisiment(Car car, string name, double price, Status saleStatus, Bitmap image)
+        {
+            Car = car;
+            Price = price;
+            SaleStatus = saleStatus;
+            CarImage = image;
+        }
+        public Car Car { get; set; }
+        public double Price { get; set; }
+        public Status SaleStatus { get; set; }
+        public Bitmap CarImage { get; set; }
+    }
+    enum Status
+    {
+        OnSale,
+        Booked,
+        Sold
+    }
+}
+
